44f83650f10ccd52426a2d4136148ed6a036a40a
Tobias G <31646748+Daikalos@users.noreply.github.com>
2021-05-01 19:47:57 +0200

    Project Creation

---
+++ Euler-Fluid/main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+#include <gl/GLU.h>
+
+int main()
+{
+	sf::Window window(sf::VideoMode(2240, 1260), "Euler Fluid");
+
+	while (window.isOpen())
+	{
+
+	}
+
+	return 0;
+}
