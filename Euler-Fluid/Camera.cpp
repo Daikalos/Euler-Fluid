e7fa9f2d5f1f74868e4cfdcf28072e55f61e5def
Tobias G <31646748+Daikalos@users.noreply.github.com>
2021-06-13 12:37:37 +0200

    utilities

---
+++ Euler-Fluid/Camera.cpp
@@ -0,0 +1,33 @@
+#include "Camera.h"
+
+Camera::Camera(const sf::Window& window) : window(window)
+{
+	position = (sf::Vector2f)window.getSize() / 2.0f;
+	scale = 1.0f;
+
+	view.setCenter(position);
+	view.setSize(size);
+	view.setViewport(sf::FloatRect(0.0f, 0.0f, 1.0f, 1.0f));
+}
+
+void Camera::update(const InputHandler& inputHandler)
+{
+	if (inputHandler.get_key_pressed(sf::Keyboard::Key::Space))
+	{
+		position = (sf::Vector2f)window.getSize() / 2.0f;
+		scale = 1.0f;
+	}
+
+	if (inputHandler.get_middle_pressed())
+		dragPos = get_mouse_world_position();
+	if (inputHandler.get_middle_held())
+		position += (sf::Vector2f)(dragPos - get_mouse_world_position());
+
+	if (inputHandler.get_scroll_down())
+		scale *= 0.85f;
+	if (inputHandler.get_scroll_up())
+		scale *= 1.15f;
+
+	view.setCenter(position);
+	view.setSize((sf::Vector2f)window.getSize() * (1.0f / scale));
+}
--- Euler-Fluid/main.cpp
+++ Euler-Fluid/main.cpp
@@ -2,13 +2,70 @@
 #include <SFML/OpenGL.hpp>
 #include <gl/GLU.h>
 
+#include "Camera.h"
+#include "InputHandler.h"
+
 int main()
 {
-	sf::Window window(sf::VideoMode(2240, 1260), "Euler Fluid");
+	sf::RenderWindow window(sf::VideoMode(2240, 1260), "Euler Fluid");
+	window.setActive(true);
 
+	sf::CircleShape circle(100.0f);
+	circle.setFillColor(sf::Color::Red);
+	Camera camera(window);
+	InputHandler inputHandler;
+
+	sf::Clock clock;
+	float deltaTime = FLT_EPSILON;
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glScalef(1.0f, -1.0f, 1.0f);
+	gluOrtho2D(0, window.getSize().x, 0, window.getSize().y);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+
+	int i = 0;
 	while (window.isOpen())
 	{
+		deltaTime = clock.restart().asSeconds();
 
+		inputHandler.update();
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::Resized:
+				glViewport(0, 0, window.getSize().x, window.getSize().y);
+				glMatrixMode(GL_PROJECTION);
+				glLoadIdentity();
+				glScalef(1.0f, -1.0f, 1.0f);
+				gluOrtho2D(0, window.getSize().x, 0, window.getSize().y);
+				glMatrixMode(GL_MODELVIEW);
+				break;
+			case sf::Event::MouseWheelScrolled:
+				inputHandler.set_scrollDelta(event.mouseWheelScroll.delta);
+				break;
+			}
+		}
+
+		camera.update(inputHandler);
+		
+		window.setView(camera.get_view());
+
+		window.clear();
+
+		window.draw(circle);
+
+		window.display();
 	}
 
 	return 0;
